a8fc58823431c88153e33dd3ef95df4282e01d0a
stepan.danilov@rtlabs.ru <stepan.danilov@rtlabs.ru>
2021-09-30 13:26:53 +0300

    init

--- sql/1_find_indiv_by_snils.sql
+++ sql/1_find_indiv_by_snils.sql
@@ -4,8 +4,27 @@ select code from indiv_code where id = (select (random() * max(id))::int from in
 --searh
 explain(verbose, analyze, buffers)
 select coalesce(
-               (select sname from indiv i where i.id  = (select indiv_id from indiv_code ic where ic.code = 'c95c304f7cb50afbfa5bc21b747f6508' and type_id = 1)), 'indiv not found'
+               (select sname from indiv i where i.id  = (select indiv_id from indiv_code ic where ic.code = 'd0cd20f38f1c73cde6db4b8ce2fcffd6' and type_id = 1)), 'indiv not found'
            );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 --drop index indiv_code_idx;
 create index indiv_code_idx on indiv_code(code)
