433a8495219a758ac8016b9cfd945ee711cfb844
stepan.danilov@rtlabs.ru <stepan.danilov@rtlabs.ru>
2021-09-30 13:52:51 +0300

    init

--- sql/1_find_indiv_by_snils.sql
+++ sql/1_find_indiv_by_snils.sql
@@ -1,11 +1,43 @@
---get random code for search in snils
+--получаем случайный идентификатор индивида
 select code from indiv_code where id = (select (random() * max(id))::int from indiv_code);
 
---searh
-explain(verbose, analyze, buffers)
+--ищем полученный код среди имеющихся СНИЛС и достаем индивида
 select coalesce(
-               (select sname from indiv i where i.id  = (select indiv_id from indiv_code ic where ic.code = 'd0cd20f38f1c73cde6db4b8ce2fcffd6' and type_id = 1)), 'indiv not found'
+    (select concat_ws(' ', sname, fname, mname) from indiv i
+        where i.id  = (select indiv_id from indiv_code ic where ic.code = 'd0cd20f38f1c73cde6db4b8ce2fcffd6' and type_id = 1)),
+    'indiv not found'
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+explain(verbose, analyse, buffers)
+select coalesce(
+               (select concat_ws(' ', sname, fname, mname) from indiv i
+                where i.id  = (select indiv_id from indiv_code ic where ic.code = 'd0cd20f38f1c73cde6db4b8ce2fcffd6' and type_id = 1)),
+               'indiv not found'
            );
+
 
 
 
