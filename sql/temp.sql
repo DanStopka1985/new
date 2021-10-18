26375a61d53971bcd7c2696ef66dc134ee53d345
stepan.danilov@rtlabs.ru <stepan.danilov@rtlabs.ru>
2021-10-18 12:33:19 +0300

    init

--- sql/temp.sql
+++ sql/temp.sql
@@ -11,3 +11,23 @@ select 'founded indivs by snils_part  : ' || coalesce((select string_agg(sname, 
 --SET max_parallel_workers_per_gather = 0;
 explain(verbose, analyze, buffers)
 select * from indiv_code ic where ic.type_id = 1 order by code limit 50
+
+
+create extension pg_trgm;
+create index gin_indiv_code_code_idx on indiv_code using gin(code gin_trgm_ops);
+drop index gin_indiv_code_code_idx
+
+explain(verbose, analyze, buffers)
+select * from indiv_code where code like '56718%'
+
+
+create table temp(id int);
+
+begin;
+create index temp_id_idx on temp using hash(id);
+rollback;
+drop index temp_id_idx
+
+explain
+select * from temp where id = 1
+
